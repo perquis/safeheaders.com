b511a4f37c86f2d8b05c010c7836ae6774b1a8ed
perquis <thetahirpl@gmail.com>
2024-09-01 12:56:04 +0200

    :lipstick: ui(root-layout): Updated RootLayout component and added component to homepage.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
-import clsx from 'clsx';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import { Background } from "@/components/background/feature-background";
+import { Footer } from "@/components/footer/feature-footer";
+import { Navigation } from "@/components/navigation/feature-navigation";
+import clsx from "clsx";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Stay Ahead of Security Risks - SafeHeaders',
+  title: "Safe Headers - Stay Ahead of Security Risks",
   description: `Scan your website to ensure it's secure. This tool checks for essential security headers every modern site should have, identifies missing headers, and provides easy-to-follow instructions to fix them.`,
 };
 
@@ -17,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={clsx(inter.className, 'bg-zinc-950 text-white')}>{children}</body>
+      <body className={clsx(inter.className, "bg-zinc-950 text-white")}>
+        <Navigation />
+        <div className="px-5 sm:px-12 md:px-24 lg:px-48 xl:px-64 2xl:px-80">
+          {children}
+        </div>
+        <Background />
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+import { HeroHeader } from "@/components/hero-header/feature-hero-header";
+import { PopularHeadersList } from "@/components/popular-headers-list/feature-popular-headers-list";
+import { PostsList } from "@/components/posts-list/feature-posts-list";
+import { Divider } from "@/shared/ui/divider/divider";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col gap-32 pb-32 pt-64">
+      <HeroHeader />
+      <div className="mx-auto flex max-w-screen-sm flex-col gap-10">
+        <PopularHeadersList />
+        <Divider />
+        <PostsList
+          posts={[
+            {
+              href: "/",
+              title: "What is a Content Security Policy?",
+              description:
+                "When protecting web applications, companies often use multiple technologies to create a layered defense.",
+            },
+            {
+              href: "/",
+              title: "What is DOM-based XSS?",
+              description:
+                "A DOM-based cross-site scripting (XSS) attack happens when a threat actor modifies the document object model (DOM) environment in the victim’s browser.",
+            },
+          ]}
+        />
+      </div>
+    </main>
+  );
 }
