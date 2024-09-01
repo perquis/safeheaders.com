d2bf766d51b65eed8e55d42aa03a5bb715656e4e
perquis <thetahirpl@gmail.com>
2024-09-01 12:48:29 +0200

    :lipstick: ui(unordered-list.tsx): Created an UnorderedList component.

---
+++ src/shared/ui/unordered-list/unordered-list.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import clsx from "clsx";
+import { FC, PropsWithChildren } from "react";
+
+interface Item {
+  label: string;
+  content: string;
+}
+
+interface IUnorderedList {
+  list: Item[];
+}
+
+export const UnorderedList: FC<IUnorderedList> = ({ list }) => {
+  return (
+    <div className="relative flex flex-col rounded-xl bg-zinc-900">
+      <img
+        src="notebook-dynamic-color.png"
+        className="absolute -right-6 -top-6"
+      />
+      {list.map((item, index) => (
+        <Item key={index}>
+          <div className="w-32 flex-shrink-0 text-zinc-500">{item.label}</div>
+          <div>{item.content}</div>
+        </Item>
+      ))}
+    </div>
+  );
+};
+
+const Item: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div
+      className={clsx(
+        "flex items-start gap-2 border-b border-zinc-800 px-4 py-3.5 text-sm font-medium last:border-0",
+      )}
+    >
+      {children}
+    </div>
+  );
+};
