fafae7f815bb4d91e75136aa9845a1f2e7827936
perquis <thetahirpl@gmail.com>
2024-09-01 14:09:21 +0200

    :seedling: seed(posts.ts): Created the posts list to use them in home page.

---
+++ src/data/mock/posts.ts
@@ -0,0 +1,14 @@
+export const posts = [
+  {
+    href: "/",
+    title: "What is a Content Security Policy?",
+    description:
+      "When protecting web applications, companies often use multiple technologies to create a layered defense.",
+  },
+  {
+    href: "/",
+    title: "What is DOM-based XSS?",
+    description:
+      "A DOM-based cross-site scripting (XSS) attack happens when a threat actor modifies the document object model (DOM) environment in the victim’s browser.",
+  },
+];
