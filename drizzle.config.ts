af1cdd1952d2e5137c20bef4ba367a7aee72e89d
perquis <thetahirpl@gmail.com>
2024-09-04 00:45:13 +0200

    :heavy_plus_sign: depadd(drizzle): Added new package to the dependecies list.

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { loadEnvConfig } from "@next/env";
+import { defineConfig } from "drizzle-kit";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./drizzle/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DRIZZLE_DATABASE_URL!,
+  },
+});
